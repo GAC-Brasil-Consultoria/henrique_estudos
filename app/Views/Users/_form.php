9b72200dd00a01cab9ad75e6f6b8a8a56d630608
hfreiregac~ <hfreire@group-gac.com.br>
2023-02-02 15:39:21 -0300

    [feat] - Criado arquivo de formulario para editar o usuario

---
+++ app/Views/Users/_form.php
@@ -0,0 +1,25 @@
+<div class="form-group">
+    <label class="form-control-label">Name</label>
+    <input type="text" name="name" placeholder="enter full name" class="form-control" value="<?php echo esc($user->name) ?>">
+</div>
+
+<div class="form-group">
+    <label class="form-control-label">Email</label>
+    <input type="email" placeholder="Email Address" class="form-control">
+</div>
+
+<div class="form-group">       
+    <label class="form-control-label">Password</label>
+    <input type="password" name="password" placeholder="Password" class="form-control">
+</div>
+
+<div class="form-group">       
+    <label class="form-control-label">Password</label>
+    <input type="password" name="password" placeholder="Password confirmation" class="form-control">
+</div>
+
+<div class="custom-control custom-checkbox">
+    <input type="hidden" name="active" value="0">
+    <input type="checkbox" name="active" value="1" class="custom-control-input" id="active" <?php if($user->active): ?>checked<?php endif; ?>>
+    <label class="custom-control-label" for="active">User active</label>
+</div>
