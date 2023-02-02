b73009eea5a5f1670046a0ebe39ba5132ca54df5
hfreiregac~ <hfreire@group-gac.com.br>
2023-02-02 14:02:11 -0300

    [feat] - Criada a View de ediçao de usuario

---
+++ app/Views/Users/edit.php
@@ -0,0 +1,42 @@
+<?php echo $this->extend('Layouts/default') ?>
+
+<?php $this->section('title') ?> <?php echo $title; ?> <?php echo $this->endSection() ?>
+
+
+<?php $this->section('styles') ?>
+
+<?php echo $this->endSection() ?>
+
+
+<?php $this->section('content') ?>
+
+<div class="row">
+
+    <div class="col-lg-4">
+        <div class="block">
+
+            <div id="response">
+
+            </div>
+
+            <?php echo form_open('/', ['id' => 'form', ['id'=>"$user->id"]]) ?>
+            
+            <div class="block-body">
+                <div class="form-group mt-5 mb-4">
+                    <input id="btn-salvar" type="submit" value="Save" class="btn btn-danger mr-2">
+                    <a href="<?php echo site_url("users/show/$user->id") ?>" class="btn btn-secondary ml-2">Back</a>
+                </div>
+            </div>
+            
+            <?php echo form_close(); ?>
+            
+        </div>
+    </div>
+</div>
+
+<?php echo $this->endSection() ?>
+
+
+<?php $this->section('scripts') ?>
+
+<?php echo $this->endSection() ?>
