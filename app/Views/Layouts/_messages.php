043f53e048bf2c90962887aac72b552cc34b61ff
hfreiregac <hfreire@group-gac.com.br>
2023-02-05 21:35:56 -0300

    [fix] - Corrigindo o nome da session

--- app/Views/Layouts/_messages.php
+++ app/Views/Layouts/_messages.php
@@ -1,7 +1,7 @@
-<?php if(session()->has('sucess')): ?>
+<?php if(session()->has('success')): ?>
 
 <div class="alert alert-success alert-dismissible fade show" role="alert">
-    <strong>Sucess!</strong> <?php echo session('sucess'); ?>
+    <strong>Success!</strong> <?php echo session('success'); ?>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
