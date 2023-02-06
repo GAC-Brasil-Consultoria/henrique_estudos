9c8bbade0d1b1d7df5fe109fcfd45d6089578ec0
hfreiregac~ <hfreire@group-gac.com.br>
2023-02-06 12:10:27 -0300

    [feat] - Criado o metodo para exibir o status de forma mais dinamica e universal

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -9,4 +9,21 @@ class User extends Entity
     
     protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
     
+    public function showStatus()
+    {
+        if($this->deleted_at != null)
+        {
+            $icon = '<span class="text-white">Deleted</span>&nbsp;<i class="fa fa-undo"></i>&nbsp;Undo';
+
+            $btnUndo = anchor("users/undodelete/$this->id", $icon, ['class' => 'btn btn-outline-danger btn-sm']);
+
+            return $btnUndo;
+        }
+        //'<i class="fa fa-unlock text-sucess"></i>&nbsp;Active' : '<i class="fa fa-unlock text-warning"></i>&nbsp;Inactive'
+        
+
+        return $this->active == true ? '<i class="fa fa-unlock text-success"></i>&nbsp;Active' :
+                                        '<i class="fa fa-lock text-warning"></i>&nbsp;Inactive';
+    }
+    
 }
