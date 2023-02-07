422517ff8f6355288d3d3d33f2578b7140557f39
hfreiregac~ <hfreire@group-gac.com.br>
2023-02-07 12:19:08 -0300

    [feat] - Adicionando metodo para exibir o status do grupo na entidade

--- app/Entities/Group.php
+++ app/Entities/Group.php
@@ -9,4 +9,21 @@ class Group extends Entity
     protected $datamap = [];
     protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
     protected $casts   = [];
+
+    public function showStatus()
+    {
+        if($this->deleted_at != null)
+        {
+            $icon = '<span class="text-white">Deleted</span>&nbsp;<i class="fa fa-undo"></i>&nbsp;Undo';
+
+            $btnUndo = anchor("groups/undodelete/$this->id", $icon, ['class' => 'btn btn-outline-danger btn-sm']);
+
+            return $btnUndo;
+        }
+        //'<i class="fa fa-unlock text-sucess"></i>&nbsp;Active' : '<i class="fa fa-unlock text-warning"></i>&nbsp;Inactive'
+        
+
+        return $this->show == true ? '<i class="fa fa-eye text-secondary"></i>&nbsp;Show group' : '<i class="fa fa-eye-slash text-danger"></i>';
+    }
+
 }
