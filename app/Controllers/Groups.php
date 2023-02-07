27e95ef64658da8de03748e67e91f9fda794f0c9
hfreiregac~ <hfreire@group-gac.com.br>
2023-02-07 17:05:06 -0300

    [feat] - Metodo update adicionado para atualizar no banco de dados

--- app/Controllers/Groups.php
+++ app/Controllers/Groups.php
@@ -84,6 +84,54 @@ class Groups extends BaseController
         return view('groups/show', $data);
     }
 
+    public function edit(int $id = null)
+    {
+        $group = $this->getGroupByID($id);
+
+        if($group->id < 3)
+            return redirect()->back()->with('info', 'Your group '.esc($group->name).' cannot be edited');
+
+        $data = [
+            'title' => "Editing group ".esc($group->name),
+            'group' => $group
+        ];
+
+        return view('Groups/edit', $data);
+    }
+
+    public function update()
+    {
+        if(!$this->request->isAJAX())
+            return redirect()->back();
+        
+        $return['token'] = csrf_hash();
+
+        $post = $this->request->getPost();
+
+        
+        
+        $group = $this->getGroupByID($post['id']);
+        
+        $group->fill($post);        
+        
+        if($group->hasChanged() == false)
+        {
+            $return['info'] = "No data to update...";
+            return $this->response->setJSON($return);
+        }
+
+        if($this->groupModel->protect(false)->save($group))
+        {
+            session()->setFlashdata('sucess', 'Data saved!');
+            return $this->response->setJSON($return);
+        }
+        
+        $return['error'] = 'Plese, check the errors below and try again';
+        $return['errors_model'] = $this->groupModel->errors();        
+        
+        return $this->response->setJSON($return);
+    }
+
     public function undodelete(int $id = null)
     {
         $group = $this->getGroupByID($id);
