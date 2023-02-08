7c6172d2d28f7af69880b241e306910fcf73917f
hfreiregac~ <hfreire@group-gac.com.br>
2023-02-08 15:13:19 -0300

    [fix] - corrigindo informações mostrada nos alertas

--- app/Views/Groups/_form.php
+++ app/Views/Groups/_form.php
@@ -12,8 +12,7 @@
 <div class="custom-control custom-checkbox">
     <input type="hidden" name="show" value="0">
     <input type="checkbox" name="show" value="1" class="custom-control-input" id="show" <?php if($group->show): ?>checked<?php endif; ?>>
-    <label class="custom-control-label" for="show">Show group access</label>
-    <a tabindex="0" style="text-decoration: none;" role="button" data-toggle="popover"
+    <label class="custom-control-label" for="show">Show group access</label><a tabindex="0" style="text-decoration: none;" role="button" data-toggle="popover"
                         data-trigger="hover" title="Important" data-content="If this group is defined as 
                         <b>Show group access</b> it will be shown as an option when defining 
                         <b>Technical Manager</b> for the Service Order">&nbsp;&nbsp;<i class="fa fa-question-circle fa-lg text-danger"></i></a>
