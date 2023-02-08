b7e2aaee55717b4a70b5f9fb89982f6208239186
hfreiregac~ <hfreire@group-gac.com.br>
2023-02-08 17:12:03 -0300

    [feat] - seeder criado para a tabela permissoes

---
+++ app/Database/Seeds/PermissionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class PermissionSeeder extends Seeder
+{
+    public function run()
+    {
+        $permissionModel = new \App\Models\PermissionsModel();
+
+        $permissions = [
+            ['name' =>  'list_users'],
+            ['name' =>  'create_users'],
+            ['name' =>  'edit_users'],
+            ['name' =>  'delete_users'],
+        ];
+
+        foreach($permissions as $perm)
+        {
+            $permissionModel->protect(false)->insert($perm);
+        }
+
+        echo 'Permissoes criadas!';
+    }
+}
