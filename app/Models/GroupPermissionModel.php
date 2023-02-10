a3ea88086ce7f2576f29c2105229e45332b9af68
hfreiregac~ <hfreire@group-gac.com.br>
2023-02-10 16:52:35 -0300

    [feat] - criado o metodo para recuperar as permissoes de grupos

--- app/Models/GroupPermissionModel.php
+++ app/Models/GroupPermissionModel.php
@@ -10,5 +10,27 @@ class GroupPermissionModel extends Model
     protected $returnType       = 'object';
     protected $allowedFields    = ['group_id', 'permission_id'];
 
-    
+    /**
+     * method to get group permissions
+     *
+     * @param integer $group_id
+     * @param integer $qtt_pages
+     * @return array|null
+     */
+    public function getPermissionsGroup(int $group_id, int $qtt_pages)
+    {
+        $atributes = [
+            'groups_permissions.id',
+            'groups.id AS group.id',
+            'permissions.id AS permission.id',
+            'permissions.name' 
+        ];
+
+        return $this->select($atributes)
+            ->join('groups', 'groups.id = groups_permissions.group_id')
+            ->join('permissions', 'permissions.id = groups_permissions.permissions_id')
+            ->where('groups_permissions.group_id', $group_id)
+            ->groupBy('permissions.name')
+            ->paginate($qtt_pages);
+    }
 }
