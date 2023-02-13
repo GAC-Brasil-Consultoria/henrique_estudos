7b8a61a1782253bba451a25126496904fc513ff9
hfreiregac <hfreire@group-gac.com.br>
2023-02-12 23:25:09 -0300

    [fix] - Alterando o nome do arquivo

--- app/Models/PermissionModel.php
+++ app/Models/PermissionModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class PermissionsModel extends Model
+class PermissionModel extends Model
 {
     
     protected $table            = 'permissions';
