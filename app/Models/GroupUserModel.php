0d2d38f51f5b121dd57a2f4cac99dcc7aff6ff0b
hfreiregac <hfreire@group-gac.com.br>
2023-02-22 13:35:04 -0300

    [refactor] - refatorando a Model de GroupUser

--- app/Models/GroupUserModel.php
+++ app/Models/GroupUserModel.php
@@ -6,37 +6,7 @@ use CodeIgniter\Model;
 
 class GroupUserModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'groupusers';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $insertID         = 0;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected $table            = 'groups_users';
+    protected $returnType       = 'object';
+    protected $allowedFields    = ['group_id', 'user_id'];
 }
