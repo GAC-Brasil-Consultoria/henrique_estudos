c6e7aa16ade8b590a8c9b10e7fd14df2b0b29332
hfreiregac <hfreire@group-gac.com.br>
2023-02-22 23:19:41 -0300

    [feat] - criado o metodo para obter os grupos de usuario do banco

--- app/Models/GroupUserModel.php
+++ app/Models/GroupUserModel.php
@@ -9,4 +9,21 @@ class GroupUserModel extends Model
     protected $table            = 'groups_users';
     protected $returnType       = 'object';
     protected $allowedFields    = ['group_id', 'user_id'];
+
+    public function getGroupsUsers(int $user_id, int $qtt)
+    {
+        $atributes = [
+            'groups_users.id',
+            'groups.id as group_id',
+            'groups.name',
+            'groups.description'
+        ];
+
+        return $this->select($atributes)
+                    ->join('groups', 'groups.id = groups_users.group_id')
+                    ->join('users', 'users.id = groups_users.user_id')
+                    ->where('groups_users.user_id', $user_id)
+                    ->groupBy('groups.name')
+                    ->paginate($qtt);
+    }
 }
